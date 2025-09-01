dfcac5a4b81667007a2a76f61e725873df32a01a
Bharath S <sbharath8900@gmail.com>
2025-09-01 19:13:42 +0530

    Add files via upload
    Day 25 of #100DaysOfCode
    Solved the "Integer to Roman" problem from LeetCode.
    Implemented a greedy algorithm to convert integers into Roman numerals.
    Handled subtractive cases like IV, IX, XL, XC, CD, and CM.
    Achieved an O(1) time solution since Roman numeral generation is fixed in size.

---
+++ #Day25/int to roman.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def intToRoman(self, num):
+      # Mapping of values to Roman numerals
+        val_to_roman = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+        ]
+
+        roman = []
+        for value, symbol in val_to_roman:
+            while num >= value:
+                roman.append(symbol)
+                num -= value
+
+        return "".join(roman)
+
+solution = Solution()
+
+print(solution.intToRoman(3749))
+print(solution.intToRoman(58))
+print(solution.intToRoman(1994))
